67c0d08e4e99886b46ed97d88911bee4153a0a00
AleexSilva <silva.alexis94@gmail.com>
2025-09-18 23:26:10 +0000

    investment calses

---
+++ Clases/porfolio.py
@@ -0,0 +1,33 @@
+class InvestmentPortdolio:
+    """
+    Class that manage the investment of a Portfolio
+    """
+    def __init__(self):
+        """
+        iniciate a portfolio investment from an empty list
+        """
+        self.investment = []
+    
+    def add_inverstment(self,asset_name,capital,expected_profitability):
+        """
+        Add a new investment to the porfolio
+        Params:
+            - asset_name: Finantial instument name (ex: AAPL, BTC)
+            - capital: investment capital for this asset
+            - expected_profitability:  pct of expected profitability fot this asset 
+        """
+        # Add investment to the porfolio
+        investment = {
+            "Asset": asset_name,
+            "capital": capital,
+            "Expected_profitability": expected_profitability 
+        }
+        
+        self.investment.append(investment)
+        print(f"Investment on {asset_name}: ${capital} wiht an expected profitability: {expected_profitability}%")
+        
+        
+    def total_profitability(self):
+        """
+        
+        """
